eba5bbdf111f254e7f499de34d4b1a610b273f9e
Ther <1700804862@qq.com>
2020-07-16 15:56:57 +0800

    修改bug

---
+++ src/components/modalHint.vue
@@ -0,0 +1,36 @@
+<template>
+  <transition name="fade">
+    <div class="modalHint" v-show="isShowModalHint">
+      {{text}}
+    </div>
+  </transition>
+</template>
+<script>
+export default {
+  props: ['text', 'isShowModalHint']
+}
+</script>
+<style scoped>
+.fade-leave-active {
+  transition: all .8s ease;
+}
+.fade-leave-to {
+  opacity: 0;
+}
+.modalHint {
+  background-color:#282828;
+  border-radius: 4px;
+  color: #fff;
+  position: absolute;
+  width: 70vw;
+  left: 50%;
+  transform: translateX(-50%);
+  top: 45%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 4vw;
+  z-index: 999;
+  padding: 0.4vh 2vw;
+}
+</style>
