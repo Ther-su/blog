ddd8b380f94f81d3605364ddab48bd117a1f71ed
Ther <1700804862@qq.com>
2020-07-17 10:46:40 +0800

    微调

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,5 @@
 module.exports = {
   devServer: {
     open: true
-  },
-  chainWebpack: config => {
-    config.set('externals', {
-      vue: 'Vue',
-      'vue-router': 'VueRouter',
-      axios: 'axios',
-      nprogress: 'NProgress'
-    })
   }
 }
